1b6615b7d0f8f3c9ef9683c679793a73b1c8229f
Lorenzo Squadrani <lorenzo.squadrani@gmail.com>
2021-05-08 19:06:18 +0200

    Added MNIST dataset

---
+++ get_dataset.py
@@ -0,0 +1,11 @@
+from torchvision import datasets, transforms
+
+
+datasets.MNIST('./', train=True, download=True,
+                   transform=transforms.Compose([
+                       transforms.ToTensor(),
+                       transforms.Normalize((0.1307,), (0.3081,))
+                   ]))
+
+
+
