78bc3b70f1e04e4492368faad98b7159c545f7e7
Lorenzo Squadrani <lorenzo.squadrani@gmail.com>
2021-05-15 21:07:36 +0200

    Improved program modularity

--- cBCM/cBCM.py
+++ cBCM/cBCM.py
@@ -23,6 +23,25 @@ class cBCM (BCM):
                                		interaction_strength = interaction_strength,
                                		precision=precision,
                                		random_state=random_state, verbose=verbose)
+
+    def _makes_slices(self, X):
+
+        N,C,W,H= X.shape
+        SN,SC,SW,SH = X.strides
+        
+        K = self.kernel_size
+        
+        view_shape = (N, W-K+1, H-K+1, C, K, K)
+        view_stride = (SN, SW, SH, SC, SW , SH)
+        
+        # Se la batch è X=(N,W,H), il kernel è (K1,K2), lo stride è 1
+        # La shape dei sottosample dev'essere (num_samples, W-K1+1, H-K2+1, K, K)
+        # Lo stride dei sottosample dev'essere 
+        # ( bytes * W * H , bytes*H * stride2   ,bytes*stride1 ,bytes * H, bytes)
+        # O, in termini dello stride di X = (SN,SW,SH):
+        # (SN, SW*stride1, SH*stride2,SW,SH)
+        
+        subs = np.lib.stride_tricks.as_strided(X, view_shape, strides=view_stride)
     
 
     def fit(self, X):
@@ -35,25 +54,13 @@ class cBCM (BCM):
                  
         
         np.random.seed(self.random_state)
+
+        K, C = self.kernel_size, self.in_channels
+
+        subs = _makes_slices(X)
         
-        N,C,W,H= X.shape
-        SN,SC,SW,SH = X.strides
-        
-        K = self.kernel_size
-        
-        view_shape = (N, W-K+1, H-K+1, C, K, K)
-        view_stride = (SN, SW, SH, SC, SW , SH)
-        
-    	# Se la batch è X=(N,W,H), il kernel è (K1,K2), lo stride è 1
-    	# La shape dei sottosample dev'essere (num_samples, W-K1+1, H-K2+1, K, K)
-    	# Lo stride dei sottosample dev'essere 
-        # ( bytes * W * H , bytes*H * stride2   ,bytes*stride1 ,bytes * H, bytes)
-    	# O, in termini dello stride di X = (SN,SW,SH):
-    	# (SN, SW*stride1, SH*stride2,SW,SH)
-		
-        subs = np.lib.stride_tricks.as_strided(X, view_shape, strides=view_stride)
-        
-        super(cBCM, self).fit(subs.reshape(N*(W-K+1)*(W-K+1), C*K*K))
+        super(cBCM, self).fit(subs.reshape(-1, C*K*K))
+ 
 
 
     def get_weights():
